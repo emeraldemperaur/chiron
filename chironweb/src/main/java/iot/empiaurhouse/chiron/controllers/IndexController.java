9755182fd691c0fdf461b5fa60d9573a5b54608f
Emeka Egwim <egwim.emeka@gmail.com>
2020-01-02 21:31:46 -0800

    Added wro4j optimizer: dependencies & properties , static resources (LESS, Fonts, Images) and Master Layout to Chiron Web Module. closes #9

--- chironweb/src/main/java/iot/empiaurhouse/chiron/controllers/IndexController.java
+++ chironweb/src/main/java/iot/empiaurhouse/chiron/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package iot.empiaurhouse.chiron.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"","/", "index", "index.html"})
-    public String index(){
+    public String index(Model indexModel){
+
+        String pattern = "E, dd MMM yyyy zzzz";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+        indexModel.addAttribute("date", date);
 
         return "index";
     }
